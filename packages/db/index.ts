1ef5b80af3a57fd8b2cf07f2be16bfaea6c3a0cf
Oliver <mustardrr4@gmail.com>
2026-06-22 09:26:46 +0100

    Remove web env dependency from db package

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client"
-import { env } from "@/env"
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -8,15 +7,13 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasources: {
-      db: {
-        url: env.DATABASE_URL,
-      },
-    },
-    log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
   })
 
-if (env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
 }
 
